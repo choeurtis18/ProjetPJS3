4f2f6eaefe7e05d3c06cdb3e4fa1b42b0bb645bd
choeurtis <choeurtis.tchounga@gmail.com>
2020-09-25 01:25:02 +0200

    AjoutJSV3

--- js/animations.js
+++ js/animations.js
@@ -1,19 +1,22 @@
 let observer = new IntersectionObserver(function (entrer) {
-    entrer.forEach(function (observable) {//selectionne les éléments de entrer
-        if(observable.intersectionRatio > 0.2){//si 50% de l'élément est "passer" 
-            observable.target.classList.add("visible") //le rendre visible
+
+    entrer.forEach(function (observable) {
+        if(observable.intersectionRatio > 0.3){
+            observable.target.classList.remove('not_visible')
             observer.unobserve(observable.target)
+            console.log("ok")
         }
     })
 
 }, {
-    threshold: [0.5] //lorsque la cible à atteint 50% de l'élément
+    threshold: [0.3] //lorsque la cible à atteint 50% de l'élément
 })
 
 
-let items = document.querySelectorAll(article) //selection le la class d'objets à observer
+let items = document.querySelectorAll(".partie") //selection le la class d'objets à observer
 
 items.forEach(function(item) {//selection des objets
+    item.classList.add('not_visible')
     observer.observe(item)
 })
 
