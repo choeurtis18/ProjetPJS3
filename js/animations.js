109895497ba1ca1bf3abd512ca6d4671f94a71f6
hte <57845968+nivadada@users.noreply.github.com>
2020-09-28 13:41:37 +0200

    commit helene
    version avec un peu de js à revoir

--- js/animations.js
+++ js/animations.js
@@ -1,22 +1,59 @@
-let observer = new IntersectionObserver(function (entrer) {
-
-    entrer.forEach(function (observable) {
-        if(observable.intersectionRatio > 0.3){
-            observable.target.classList.remove('not_visible')
-            observer.unobserve(observable.target)
-            console.log("ok")
-        }
+(function (){
+    let observerPage = new IntersectionObserver(function (observables) {
+        observables.forEach(function (observable) {
+            // L'élément devient visible
+            if (observable.intersectionRatio > 0.3) {
+                observable.target.classList.remove('not_visible')
+            }
+        })
+        observer.unobserve(observable.target)
+    }, 
+    {
+        threshold: [0.3]
+    });
+    
+    let observerZoom = new IntersectionObserver(function (observables) {
+        observables.forEach(function (observable) {
+            // Zoom sur l'element
+            if (observable.intersectionRatio > 0.3) {
+                observable.target.classList.add('zoom')
+            }
+        })
+        observer.unobserve(observable.target)
+    }, 
+    {
+        threshold: [0.3]
+    });
+    
+    let observerApparition = new IntersectionObserver(function (observables) {
+        observables.forEach(function (observable) {
+            // Zoom sur l'element
+            if (observable.intersectionRatio > 0.3) {
+                observable.target.classList.add('apparition')
+            }
+        })
+        observer.unobserve(observable.target)
+    }, 
+    {
+        threshold: [0.3]
+    });
+    
+    // On observe nos éléments
+    let apparaitre = document.querySelectorAll('.apparaitre')
+    apparaitre.forEach(function (item) {
+        item.classList.add('not_visible')
+        observerPage.observe(item)
     })
-
-}, {
-    threshold: [0.3] //lorsque la cible à atteint 50% de l'élément
-})
-
-
-let items = document.querySelectorAll(".partie") //selection le la class d'objets à observer
-
-items.forEach(function(item) {//selection des objets
-    item.classList.add('not_visible')
-    observer.observe(item)
-})
-
+    let texte_banniere = document.querySelectorAll('.texte_banniere')
+    texte_banniere.forEach(function (item) {
+        observerApparition.observe(item)
+    })
+    let diplome = document.querySelectorAll('.diplome')
+    diplome.forEach(function (item) {
+        observerZoom.observe(item)
+    })
+    let BoxiutEnChiffre = document.querySelectorAll('.BoxiutEnChiffre')
+    BoxiutEnChiffre.forEach(function (item) {
+        observerZoom.observe(item)
+    })
+})()
